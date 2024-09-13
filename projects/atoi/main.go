b6afd96a02f3c02388d51900abe4d87fc722e260
gosha <goshavlasov2005@gmail.com>
2024-09-13 13:47:23 +0300

    solved

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var num int
+	fmt.Scan(&num)
+	var queue []int
+	for num > 0 {
+		a := num % 10
+		queue = append(queue, a*a)
+		num = num / 10
+	}
+	for i := 0; i < len(queue); i++ {
+		fmt.Print(queue[len(queue)-i-1])
+	}
 }
--- projects/star/main.go
+++ projects/star/main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var s, ans string
+	fmt.Scan(&s)
+	ans = ""
+	for i := 0; i < len(s)-1; i++ {
+		ans += string(s[i]) + "*"
+	}
+	ans += s[len(s)-1:]
+	fmt.Println(ans)
 }
